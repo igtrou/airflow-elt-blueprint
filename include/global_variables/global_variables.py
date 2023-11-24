48cd58671e3e8482555f8a71b4666f8eb10b5bc5
igtrou <92401040+igtrou@users.noreply.github.com>
2023-11-24 12:26:51 +0000

    "Primeira modificação ---test"

--- include/global_variables/global_variables.py
+++ include/global_variables/global_variables.py
@@ -13,8 +13,8 @@ import json
 # Enter your own info! #
 # -------------------- #
 
-MY_NAME = "Friend"
-MY_CITY = "Portland"
+MY_NAME = "Igor"
+MY_CITY = "Florestal"
 
 # ----------------------- #
 # Configuration variables #
